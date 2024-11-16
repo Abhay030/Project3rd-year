d14a96779055b59c551e98b88c8e8e8f316cb7c1
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2024-11-16 11:16:04 +0000

    main files

---
+++ frontend/src/Front.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Front = () => {
+    return (
+        <div>
+            <h1>Welcome to the Front Page</h1>
+            <p>This is a React component rendered using JSX.</p>
+        </div>
+    );
+};
+
+export default Front;
